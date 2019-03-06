b789bb20a1ce245cdd1d73385e25c5dfbfbd7d9a
maheshwagh29 <maheshw@leotechnosoft.net>
2019-03-06 20:17:58 +0530

    test

--- src/com/juniper/pages/Administration_Page.java
+++ src/com/juniper/pages/Administration_Page.java
@@ -8,7 +8,7 @@ public class Administration_Page {
 	@FindBy(xpath="//*[@id='homepage-navigation']/li[5]/a")
 	public WebElement AdministrationClick;
 	
-	//Test
+	//Test test test
 	@FindBy(xpath="//a[@href='/juniper-sh-qa-c-20/Administration/Users']")
 	public WebElement UserAdministrationClick;
 	
